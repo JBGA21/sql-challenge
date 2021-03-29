fd76c0d2769e6bc36a0d42ead22f37356f8899c5
JBGA21 <jesusbeltran_93@hotmail.com>
2021-03-29 09:07:26 -0600

    Instructions for query #8 added

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -50,3 +50,4 @@ INNER JOIN departments AS d
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
 ORDER BY 4;
 
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
