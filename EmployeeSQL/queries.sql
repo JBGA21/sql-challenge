fbfe4f5728e8859160d98d37419dceb7a3f17c8d
JBGA21 <jesusbeltran_93@hotmail.com>
2021-03-29 09:06:13 -0600

    Multiple departments query added

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -41,3 +41,12 @@ INNER JOIN departments AS d
 WHERE d.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS dpe
+	ON dpe.emp_no = e.emp_no
+INNER JOIN departments AS d
+	ON d.dept_no = dpe.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY 4;
+
