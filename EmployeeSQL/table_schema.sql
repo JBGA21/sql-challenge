cf731793ca4a4f9a610915884670f04c5f15ef93
JBGA21 <jesusbeltran_93@hotmail.com>
2021-03-29 07:09:25 -0600

    Tables Schema added

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,49 @@
+-- Create titles table
+CREATE TABLE titles (
+	title_id INTEGER,
+	title VARCHAR,
+	PRIMARY KEY (title_id)
+);
+
+-- Create employees table
+CREATE TABLE employees (
+	emp_no INTEGER,
+	emp_title_id INTEGER,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create salaries table
+CREATE TABLE salaries (
+	emp_no INTEGER,
+	salary INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no) 
+);
+
+-- Create departments table
+CREATE TABLE departments (
+	dept_no INTEGER,
+	dept_name VARCHAR,
+	PRIMARY KEY (dept_no)
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager(
+	dept_no INTEGER,
+	emp_no INTEGER,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INTEGER,
+	dept_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
