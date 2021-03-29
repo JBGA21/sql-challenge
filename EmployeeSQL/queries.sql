08292723cd7162a0a90f4c293868645b6a80b061
JBGA21 <jesusbeltran_93@hotmail.com>
2021-03-29 09:10:54 -0600

    Last name count query added

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -51,3 +51,7 @@ WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
 ORDER BY 4;
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "last_name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY last_name_count DESC;
